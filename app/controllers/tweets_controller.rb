34f5860b7462a5900a466b99d42541f08095a695
Jesse Wenger <wengerj@gmail.com>
2021-04-22 21:07:26 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -42,27 +42,27 @@ class TweetsController < ApplicationController
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
       @tweet = Tweet.find_by(id: params[:id])
-      erb :'tweets/show'
+      erb :'tweets/edit'
     else
       redirect to 'login'
     end
   end
 
-  patch '/tweet/edit/:id' do
-    if !params[:content].empty?
+  patch '/tweets/:id' do
+    if !params[:content].empty? || Helpers.is_logged_in?(session)
       user = Helpers.current_user(session)
-      @tweet = Tweet.new(content: params[:content], user_id: user.id)
-      @tweet.save
-      redirect to "tweets/#{@tweet.id}"
+      @tweet = Tweet.find_by(id: params[:id])
+      @tweet.update(content: params[:content])
+      redirect to "tweets/#{@tweet.id}/edit"
     end
   end
 
-  delete '/tweet/:id' do
-    if Helpers.is_logged_in?(session)
-      @user = Helpers.current_user(session)
-      @tweet = Tweet.find_by(id: params[:id])
+  delete '/tweets/:id' do
+    @tweet = Tweet.find_by(id: params[:id])
+    @user = Helpers.current_user(session)
+    if Helpers.is_logged_in?(session) && @user == @tweet.user
       @tweet.delete
-      erb :'tweets'
+      redirect to "users/show"
     else
       redirect to 'login'
     end
