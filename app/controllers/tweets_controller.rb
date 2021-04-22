ec51cff0a3a4645efbf484664bfbd304f04349ed
Jesse Wenger <wengerj@gmail.com>
2021-04-22 14:57:40 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -48,7 +48,7 @@ class TweetsController < ApplicationController
     end
   end
 
-  patch '/tweet/:id' do
+  patch '/tweets' do
     if !params[:content].empty?
       user = Helpers.current_user(session)
       @tweet = Tweet.new(content: params[:content], user_id: user.id)
